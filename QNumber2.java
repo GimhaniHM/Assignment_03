5243f73200fbb4eef80c4e4639b7910ddc179eb4
GimahniHM <gimhaniharshika99@gmail.com>
2021-09-03 15:43:00 +0500

    After change QNumber2

--- QNumber2.java
+++ QNumber2.java
@@ -1,12 +1,12 @@
 public class QNumber2{
 	public static void main(String[] args){
-		int arry[] = {-23,-6,-47,-35,-2,-14};
-		int min=arry[0],max=arry[0];
-		for(int i=0;i<arry.length;i++){
-			if(arry[i]<min)
-				min=arry[i];
-			else if(arry[i]>max)
-				max=arry[i];	
+		int array[] = {-23,-6,-47,-35,-2,-14};
+		int min=array[0],max=array[0];
+		for(int i=0;i<array.length;i++){
+			if(array[i]<min)
+				min=array[i];
+			else if(array[i]>max)
+				max=array[i];	
 		}
 		System.out.print("\nLowest value : "+min);
 		System.out.print("\nHighest value : "+max+"\n");
